cc6490f9dffa0b558f13408e71e1fb4abd1b729a
Nobody <mishasikolenko@mail.ru>
2023-07-03 21:33:54 +0300

    OneMax_Deap

--- Native OneMax/native_OneMax.py
+++ Native OneMax/native_OneMax.py
@@ -1,9 +1,9 @@
 import random
 import matplotlib.pyplot as plt
 
-ONE_MAX_LENGTH = 200   
+ONE_MAX_LENGTH = 100   
 
-POPULATION_SIZE = 100   
+POPULATION_SIZE = 1000   
 P_CROSSOVER = 0.9       
 P_MUTATION = 0.1        
 MAX_GENERATIONS = 500    
@@ -36,7 +36,7 @@ def selTournament(population, p_len):
     for n in range(p_len):
         i1 = i2 = i3 = 0
         while i1 == i2 or i1 == i3 or i2 == i3:
-            i1, i2, i3 = random.randint(0, p_len-1), random.randint(0, p_len-1), random.randint(0, p_len-1)
+            i1, i2, i3 = random.randint(0, p_len-1), random.randint(0, p_len-1), random.randint(0, p_len - 1)
 
         offspring.append(max([population[i1], population[i2], population[i3]], key=lambda ind: ind.fitness.values[0]))
 
